cb7fbe0cf345b4bdcae1857f026c6ec9073cd4fa
shahriar-hossain <shahriar9412@gmail.com>
2023-09-19 17:27:02 +0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import EventHandler from './EventHandler'
+
+const App = () => {
+  return (
+    <div>
+        <h1>Event Handler</h1>
+        <EventHandler />
+    </div>
+  )
+}
+
+export default App
